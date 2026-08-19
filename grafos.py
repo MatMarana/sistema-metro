50b84d012a78a33f7968dc67d5edcf96fb509c6f
Mateus Marana <assumateus@gmail.com>
2026-08-19 11:09:17 -0300

    fix[remover vértices] && feat[adicionar ligações]

--- grafos.py
+++ grafos.py
@@ -10,3 +10,12 @@ class Grafo:
     def remover_estacao(estacao):
         if estacao in self.linha_metro.keys():
             del self.linha_metro[estacao]
+            for _,ligacoes in self.linha_metro.items():
+                if estacao.nome in ligacoes:
+                    ligacoes.remove(estacao.nome)
+
+    def adicionar_ligacoes(estacao1, estacao2):
+        if (estacao1 not in self.linha_metro.keys()) and (estacao2 not in self.linha_metro.keys()):
+            return
+
+        
